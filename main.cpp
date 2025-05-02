3e4e89d87fde14bf0544b44a291dcac156ef9eb8
Arroml <107889797+Arroml@users.noreply.github.com>
2025-05-02 12:12:53 +0200

    loop

--- main.cpp
+++ main.cpp
@@ -5,36 +5,54 @@
 //#include "stack"
 
 
-int main()
-{
+int main() {
     int eingabe;
     std::string filename;
-    do{
-        std::cout <<"1       show File\n"
-                  <<"0      Programm beenden\n";
-        std::cin>>eingabe;
-        switch (eingabe) {
-        case 1:
-        {
-            XMLLoader loader;
+    std::string files;
 
-            std::cout<<"Filename (without datatype): ";
-            std::cin>>filename;
-            try {
-                loader.load("../../XML_test_files/"+filename+".xml");
-            } catch (std::runtime_error er) {
-                std::cerr<<er.what();
+
+    do {
+        std::cout<<"1       show all Files\n"
+                << "2       show specific File\n"
+                << "0      Programm beenden\n";
+        std::cin >> eingabe;
+        switch (eingabe) {
+            case 1: {
+                XMLLoader loader;
+                for (const auto &entry: std::filesystem::directory_iterator("../XML_test_files/")) {
+                    if (entry.path().extension() == ".xml") {
+                        std::cout << entry.path().string() << std::endl << std::endl;
+                        try {
+                            loader.load(entry.path().string());
+                            std::cout << std::endl << std::endl;
+                        } catch (std::runtime_error er) {
+                            std::cerr << er.what();
+                        }
+                    }
+                }
+                break;
             }
-            break;
+
+            case 2: {
+                XMLLoader loader;
+
+                std::cout << "Filename (without datatype): ";
+                std::cin >> filename;
+                try {
+                    loader.load("../XML_test_files/" + filename + ".xml");
+                } catch (std::runtime_error er) {
+                    std::cerr << er.what();
+                }
+                break;
+            }
+            case 0:
+                std::cout << "Programm wird beendet\n";
+                break;
+            default:
+                std::cout << "ungeültige Eingabe";
+                break;
         }
-        case 0:
-            std::cout<<"Programm wird beendet\n";
-            break;
-        default:
-            std::cout<<"ungeültige Eingabe";
-            break;
-        }
-    }while(eingabe);
+    } while (eingabe);
 
     return 0;
 }
